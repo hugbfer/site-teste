27fb81c78aab6652a7ce75f95c64f6300c2e9951
Hugo Ferreira <hugof508@gmail.com>
2025-01-27 13:03:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Captura o botão e o áudio
-const playButton = document.getElementById('playButton');
-const audioPlayer = document.getElementById('audioPlayer');
+const playButton = document.getElementById("playButton");
+const audioPlayer = document.getElementById("audioPlayer");
+
+const conteudo = document.getElementById("conteudo");
+
+const mensagem = document.getElementById("mensagem");
 
 // Adiciona um evento de clique no botão
-playButton.addEventListener('click', () => {
-    // Reproduz a música e começa o loop
-    audioPlayer.play();
+playButton.addEventListener("click", () => {
+  // Garantir que o áudio comece a tocar em loop quando o botão for clicado
+  audioPlayer.play().catch((error) => {
+    // Se houver erro (por exemplo, bloqueio de reprodução automática), loga o erro
+    console.error("Erro ao tentar tocar o áudio:", error);
+  });
+  mensagem.classList.remove("d-none");
+  mensagem.classList.add("d-block");
+
+  conteudo.classList.remove("d-block");
+  conteudo.classList.add("d-none");
 });
